14590e406fc28d537f53b9557c7a84d0738b1218
Kevin Audleman <kevin.audleman@gmail.com>
2018-03-03 16:26:23 -0800

    maybe

--- src/utils.groovy
+++ src/utils.groovy
@@ -1,6 +1,9 @@
 import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
+def helloWorld(){
+    println "hello world"
+}
 
 // wip - http://www.joergm.com/2010/09/executing-shell-commands-in-groovy/
 def pyCommand(String command) {
