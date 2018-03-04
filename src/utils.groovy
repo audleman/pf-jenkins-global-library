890f87193671a41917418d6400fbbafc77e93638
Kevin Audleman <kevin.audleman@gmail.com>
2018-03-03 16:18:06 -0800

    wef

--- src/utils.groovy
+++ src/utils.groovy
@@ -19,31 +19,31 @@ def pyCommand(String command, env) {
     println "this works"
     return
     // Write string to a temp file in the workspace
-    writeFile(file: "tmp.py", text: command)
-    fullPath = env.WORKSPACE + "/tmp.py"
+    // writeFile(file: "tmp.py", text: command)
+    // fullPath = env.WORKSPACE + "/tmp.py"
 
-    // fullPath = "c:/Program Files (x86)/Jenkins/workspace/test2/tmp.py"
+    // // fullPath = "c:/Program Files (x86)/Jenkins/workspace/test2/tmp.py"
 
-    // Use the low-level process builder. This is so we can:
-    // - capture regular output
-    // - capture exit code so we know if an exception occurred
-    // - redirect stderr to stdout so we can print a stack trace
-    def process = new ProcessBuilder([ "C:/Python27/python.exe", "-u", fullPath ])
-        .redirectErrorStream(true)
-        .start()
-    // Read output into a string builder
-    StringBuilder builder = new StringBuilder();
-    process.inputStream.eachLine {
-        builder.append(it)
-        builder.append(System.getProperty("line.separator"));
-    }
-    process.waitFor();
-    output = builder.toString()
-    if (process.exitValue() != 0){
-        throw new Exception(output)
-    }
-    // Success, return stdout
-    return output 
+    // // Use the low-level process builder. This is so we can:
+    // // - capture regular output
+    // // - capture exit code so we know if an exception occurred
+    // // - redirect stderr to stdout so we can print a stack trace
+    // def process = new ProcessBuilder([ "C:/Python27/python.exe", "-u", fullPath ])
+    //     .redirectErrorStream(true)
+    //     .start()
+    // // Read output into a string builder
+    // StringBuilder builder = new StringBuilder();
+    // process.inputStream.eachLine {
+    //     builder.append(it)
+    //     builder.append(System.getProperty("line.separator"));
+    // }
+    // process.waitFor();
+    // output = builder.toString()
+    // if (process.exitValue() != 0){
+    //     throw new Exception(output)
+    // }
+    // // Success, return stdout
+    // return output 
      
 }
 ///
