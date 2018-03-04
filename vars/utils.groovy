b0692d4ce39ef4cd7cf03079de642c328db4c179
Kevin Audleman <kevin.audleman@gmail.com>
2018-03-03 17:21:27 -0800

    hello

--- vars/utils.groovy
+++ vars/utils.groovy
@@ -30,7 +30,7 @@ def pyCommand(script, command) {
     process.waitFor();
     // output = builder.toString()
     if (process.exitValue() != 0){
-        throw new Exception(output)
+        throw new Exception("output")
     }
     // Success, return stdout
     return "output soon" 
